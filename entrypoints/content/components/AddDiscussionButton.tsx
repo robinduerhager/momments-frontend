9cdae5cdb20c61a0d607a1250d6e7c30e9e91f91
Robin Dürhager <robin.duerhager@gmail.com>
2025-03-02 19:06:12 +0100

    refactor addCommentButton to addDiscussionButton since it places discussions, not comments

---
+++ entrypoints/content/components/AddDiscussionButton.tsx
@@ -0,0 +1,14 @@
+import { FaSolidPlus } from 'solid-icons/fa'
+import { setMommentsStore } from '../store'
+
+export const AddDiscussionButton = () => {
+    const activateDiscussionPlacementMode = () => {
+        setMommentsStore('placeNewDiscussionMode', true)
+    }
+
+    return (
+        <button class={`cursor-pointer bg-green-400 w-[64px] h-[64px] rounded-full flex justify-center items-center`} onClick={activateDiscussionPlacementMode}>
+            <FaSolidPlus size={32} color="#ffffff" />
+        </button>
+    )
+}
