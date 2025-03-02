31e6cc29f80c843510eb0cead6ba109fe4e19af2
Robin Dürhager <robin.duerhager@gmail.com>
2025-03-02 19:05:07 +0100

    refactor store with typings

--- entrypoints/content/store.ts
+++ entrypoints/content/store.ts
@@ -1,12 +1,30 @@
 import { createStore } from 'solid-js/store'
+import { DiscussionDTO } from '$/services'
 
-export const [mommentsStore, setMommentsStore] = createStore({
+type MommentsStore = {
+    user: {
+        token: string
+    }
+    discussions: {
+        list: DiscussionDTO[]
+        active?: DiscussionDTO
+    }
+    appActive: boolean
+    placeNewDiscussionMode: boolean
+}
+
+export const [mommentsStore, setMommentsStore] = createStore<MommentsStore>({
     user: {
         token: '',
     },            // The user that is currently logged in
     discussions: {
-        discussionList: [],       // All the discussions that are available in the workspace
-        activeDiscussion: {},     // The currently active discussion in the workspace
+        list: [],       // All the discussions that are available in the workspace
+        active: undefined,     // The currently active discussion in the workspace
     },
     appActive: false,    // Whether the Discussion Mode is active in the workspace
+    placeNewDiscussionMode: false
   })
+
+
+// Nested store for better managing discussions
+export const [discussions, setDiscussions] = createStore(mommentsStore.discussions)
