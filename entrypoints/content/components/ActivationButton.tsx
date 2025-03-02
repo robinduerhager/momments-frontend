0539cf1de2f5b8d75878fead7383c37f88033fd4
Robin Dürhager <robin.duerhager@gmail.com>
2025-03-02 19:06:45 +0100

    add Awareness element to ActivationButton

--- entrypoints/content/components/ActivationButton.tsx
+++ entrypoints/content/components/ActivationButton.tsx
@@ -1,12 +1,23 @@
 import { Show } from 'solid-js';
-import { mommentsStore, setMommentsStore } from '@/entrypoints/content/store';
 import { FaSolidComment, FaSolidXmark } from "solid-icons/fa";
+import { mommentsStore, setMommentsStore, setDiscussions } from '$/store';
+import { DiscussionService } from '$/services';
 
 export const ActivationButton = () => {
+
+    const handleClick = async () => {
+        const discussions = await DiscussionService.getDiscussions()
+        setDiscussions('list', discussions)
+        setMommentsStore('appActive', !mommentsStore.appActive)
+    }
+
     return (
-        <button class={`cursor-pointer ${mommentsStore.appActive ? 'bg-red-400' : 'bg-blue-400'} ${mommentsStore.appActive ? 'w-[50px] h-[50px]' : 'w-[64px] h-[64px]'} rounded-full flex justify-center items-center`} onClick={() => setMommentsStore('appActive', !mommentsStore.appActive)}>
-            <Show when={mommentsStore.appActive} fallback={<FaSolidComment size={32} color="#ffffff" />}>
-                <FaSolidXmark size={32} color="#ffffff" />
+        <button class={`cursor-pointer relative ${mommentsStore.appActive ? 'bg-red-400' : 'bg-blue-400'} ${mommentsStore.appActive ? 'w-[50px] h-[50px]' : 'w-[64px] h-[64px]'} rounded-full flex justify-center items-center`} onClick={handleClick}>
+            <Show when={!mommentsStore.appActive} fallback={<FaSolidXmark size={32} color="#ffffff" />}>
+                <FaSolidComment size={32} color="#ffffff" />
+                <div class='w-[20px] h-[20px] rounded-full absolute bottom-0 right-0 bg-green-500'>
+                    <div class='discussion-activation-awareness-animation'></div>
+                </div>
             </Show>
         </button>
     )
