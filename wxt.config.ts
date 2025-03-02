4ba0d8d2502b02c131ba3e8ed00ac17fe957a955
Robin Dürhager <robin.duerhager@gmail.com>
2025-03-02 18:59:46 +0100

    add better path alias

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig, WxtViteConfig } from 'wxt';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-solid'],
+  alias: {
+    '$': path.resolve(__dirname, 'entrypoints/content'),
+  },
   vite: () => ({
     plugins: [tailwindcss()],
   }) as WxtViteConfig,
